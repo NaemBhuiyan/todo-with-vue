c23ca861384e715e5e332dd67d745d8f01a2818a
Naem Bhuiyan <hopingboynaem@gmail.com>
2021-02-04 17:19:29 +0600

    filter

--- src/views/Country.vue
+++ src/views/Country.vue
@@ -14,19 +14,22 @@
       </v-col>
     </v-row>
 
-    <v-card class="mx-auto mb-5" max-width="400" v-if="country">
-      <v-img class="white--text align-end" height="200px" :src="country.flag">
-      </v-img>
-      <v-card-title class="light-blue"> Name: {{ country.name }}</v-card-title>
-      <v-card-text class="text--primary text-left light-blue">
-        <p class="font-weight-black">Capital: {{ country.capital }}</p>
-        <p class="font-weight-black">
-          Population: {{ new Intl.NumberFormat().format(country.population) }}
-        </p>
-        <p class="font-weight-black">Region: {{ country.region }}</p>
-      </v-card-text>
-    </v-card>
-
+    <div v-for="country in countries" v-bind:key="country.name">
+      <v-card class="mx-auto mb-5" max-width="400" v-if="country">
+        <v-img class="white--text align-end" height="200px" :src="country.flag">
+        </v-img>
+        <v-card-title class="light-blue">
+          Name: {{ country.name }}</v-card-title
+        >
+        <v-card-text class="text--primary text-left light-blue">
+          <p class="font-weight-black">Capital: {{ country.capital }}</p>
+          <p class="font-weight-black">
+            Population: {{ new Intl.NumberFormat().format(country.population) }}
+          </p>
+          <p class="font-weight-black">Region: {{ country.region }}</p>
+        </v-card-text>
+      </v-card>
+    </div>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
@@ -40,7 +43,7 @@ export default {
     return {
       data: [],
       name: "",
-      country: null,
+      countries: [],
     };
   },
   created() {
@@ -53,10 +56,9 @@ export default {
   },
   methods: {
     submit(name) {
-      this.country = this.data.find((country) =>
+      this.countries = this.data.filter((country) =>
         country.name.toLowerCase().includes(name)
       );
-      console.log(this.data);
       this.name = "";
     },
   },
